afa440c2522db17c3cfd3b92638c9ff62a7474c1
Darshil-154 <98377822+DarshilNaliyapara@users.noreply.github.com>
2025-01-02 13:17:41 +0530

    Add files via upload
    new

---
+++ multithreading.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class multithreading {
+
+    public static void main(String[] args) {
+        Scanner name = new Scanner();
+        System.out.println("this is empty");
+    }
+}
